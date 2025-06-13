64f60222fbe5e8957c39c63e91aedc0219b5227e
Sriram <141415831+Sriram1316@users.noreply.github.com>
2025-06-13 13:25:49 +0530

    Create lcd_code.ino

---
+++ ardinuo_codes_basics/lcd_code.ino
@@ -0,0 +1,14 @@
+//display the word in lcd 
+#include<LiquidCrystal.h>
+  LiquidCrystalcc(12,11,5,4,6,7);
+void setup()
+{
+ lcd.begin(16,2);
+lcd.print("hello sriram how you do");
+delay(1000);
+}
+ void loop()
+{
+}
+
+    
